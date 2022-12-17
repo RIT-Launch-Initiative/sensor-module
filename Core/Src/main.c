2d0cf10618b20fdfbc955620858114984aef6996
Aaron Chan <aarchan108@gmail.com>
2022-12-16 23:41:58 -0500

    Update Makefile

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,7 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-
+#include "device/peripherals/LED/led.h"
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
