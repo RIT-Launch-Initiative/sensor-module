d9c5d76485fb2a77379bf85f9aec05bcfa389a2b
Aaron Chan <aarchan108@gmail.com>
2023-02-26 12:50:03 -0500

    Updated scheduler calls with recent changes to launch-core

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -83,7 +83,15 @@ static HALI2CDevice *i2cDev = nullptr;
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-RetType ledTask() {
+RetType i2cDevPollTask(void*) {
+    RESUME();
+    CALL(i2cDev->poll());
+    RESET();
+
+    return RET_SUCCESS;
+}
+
+RetType ledTask(void*) {
     RESUME();
 
     RetType ret = CALL(led->toggle());
@@ -92,7 +100,7 @@ RetType ledTask() {
     return RET_SUCCESS;
 }
 
-RetType bmpTask() {
+RetType bmpTask(void*) {
     RESUME();
 
 //    RetType ret = CALL(bmp390->pullSensorData());
@@ -102,7 +110,7 @@ RetType bmpTask() {
 }
 
 // TODO: Figure out the initialization task
-RetType sensorInitTask() {
+RetType sensorInitTask(void*) {
     RESUME();
 
 
@@ -113,7 +121,7 @@ RetType sensorInitTask() {
     if (ret != RET_ERROR) {
         CALL(uartDev->write((uint8_t *) "LED Success Init\r\n", 18));
 
-        ledTID = sched_start(&ledTask);
+        ledTID = sched_start(ledTask, {});
         if (-1 == ledTID) {
             CALL(uartDev->write((uint8_t *) "Failed to init LED task\n\r", 25));
         }
@@ -128,7 +136,7 @@ RetType sensorInitTask() {
     if (bmp390Ret == RET_ERROR) {
         CALL(uartDev->write((uint8_t *) "BMP Failed to Initialize\r\n", 26));
     } else {
-        bmp390TID = sched_start(bmpTask);
+        bmp390TID = sched_start(bmpTask, {});
 
         if (-1 == bmp390TID) {
             CALL(uartDev->write((uint8_t *) "BMP Task Startup Failed\n\r", 25));
@@ -207,7 +215,8 @@ int main(void) {
     }
 
     i2cDev = &i2c;
-    sched_start(sensorInitTask);
+    sched_start(i2cDevPollTask, {});
+    sched_start(sensorInitTask, {});
 
   /* USER CODE END 2 */
 
