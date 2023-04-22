685ee19b192bc67c52f4f5c9f4c6a06e03017d38
Torin Samples <dragonclaw339@gmail.com>
2023-04-22 14:30:10 -0400

    feature

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -135,7 +135,7 @@ RetType adxlTask(void*) {
     static int gCounter = 0;
     static int deceleration = 0;
 
-    static ADXL375_VELOCITY_FILTER_T xFilterCoords;
+    static   xFilterCoords;
     static ADXL375_VELOCITY_FILTER_T yFilterCoords;
     static ADXL375_VELOCITY_FILTER_T zFilterCoords;
 
